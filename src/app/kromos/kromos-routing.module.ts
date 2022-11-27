5ce10837bc339e88bfc464f9d5c71f0a754090c4
tonimjdev <tonimdev@gmail.com>
2022-11-27 19:56:56 +0100

    Barra navegacion, creacion componentes hijos de mycollection, enrutamientos & specials.json

--- src/app/kromos/kromos-routing.module.ts
+++ src/app/kromos/kromos-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CountriesComponent } from './mycollection/countries/countries.component';
 import { MycollectionComponent } from './mycollection/mycollection.component';
+import { SpecialsComponent } from './mycollection/specials/specials.component';
+import { TimelineComponent } from './mycollection/timeline/timeline.component';
 
 const routes: Routes = [
 
   {
     path: '',
     children: [
-      { path: '', component: MycollectionComponent },
+      { path: '', component: MycollectionComponent, },
+      { path: 'specials', component: SpecialsComponent, },
+      { path: 'countries', component: CountriesComponent, },
+      { path: 'timeline', component: TimelineComponent, },
       { path: '**', redirectTo: '' },
     ]
   }
--- src/app/kromos/kromos.module.ts
+++ src/app/kromos/kromos.module.ts
@@ -1,22 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 
 import { MycollectionComponent } from './mycollection/mycollection.component';
 import { KromosRoutingModule } from './kromos-routing.module';
 import { ProgressbarComponent } from './components/progressbar/progressbar.component';
-import { SharedModule } from '../shared/shared.module';
+
+import { SpecialsComponent } from './mycollection/specials/specials.component';
+import { CountriesComponent } from './mycollection/countries/countries.component';
+import { TimelineComponent } from './mycollection/timeline/timeline.component';
+
 
 
 
 @NgModule({
   declarations: [
     MycollectionComponent,
-    ProgressbarComponent
+    ProgressbarComponent,
+    SpecialsComponent,
+    CountriesComponent,
+    TimelineComponent
   ],
   imports: [
     CommonModule,
+    RouterModule,
     KromosRoutingModule,
     SharedModule
   ]

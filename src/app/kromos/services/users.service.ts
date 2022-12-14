527aab0b74d5565606a302ea4a282b162e5f5132
tonimjdev <tonimdev@gmail.com>
2022-12-14 16:14:38 +0100

    Cambiar API Geolicalización

--- src/app/kromos/services/users.service.ts
+++ src/app/kromos/services/users.service.ts
@@ -9,13 +9,22 @@ export class UsersService {
   constructor( private http: HttpClient ) { }
 
   public ubicacion: any[] = [];
+  private url: string = "https://api.geoapify.com/v1/geocode/";
+  private apiKey: string = "369dd5b741084e1184ed7e12584025a8";
 
   geolocalizarUser( lat:number, lon:number ) {
     this.ubicacion = [];
-    this.http.get(`https://geocode.xyz/${lat},${lon}?geoit=json`)
+    this.http.get(`${this.url}reverse?lat=${lat}&lon=${lon}&apiKey=${this.apiKey}`)
     .subscribe( (resp:any) => {
-      //console.log(resp);
-      this.ubicacion = [ resp.postal, resp.city ];
+      console.log(resp.features[0].properties);
+
+      let barri = ""
+
+      if (resp.features[0].properties.district === undefined) {barri=resp.features[0].properties.suburb}
+      else {barri=resp.features[0].properties.district};
+      this.ubicacion = [ barri,
+                        resp.features[0].properties.postcode,
+                        resp.features[0].properties.city];
     })
   }
 }
